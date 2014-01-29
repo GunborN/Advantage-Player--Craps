4465cdd2e4af0713f615056117cbbe82fc31139a
yrokhlin <yrokhlin@gmail.com>
2014-01-29 16:07:43 -0500

    Added playerProfile class

---
+++ Advantage Player: Craps/playerProfile.h
@@ -0,0 +1,13 @@
+//
+//  playerProfile.h
+//  Advantage Player: Craps
+//
+//  Created by Yevgeniy Rokhlin on 1/29/14.
+//  Copyright (c) 2014 IXLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface playerProfile : NSObject
+
+@end
